72865324026bd86b1a2b92ce0229b4e9ee6a1668
Harshit Jain <harshit.jain@macys.com>
2020-05-12 00:54:55 -0500

    first commit

---
+++ src/main/java/com/example/demo/controllers/ProductServiceController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controllers;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.beans.Product;
+
+
+@RestController
+public class ProductServiceController {
+   private static Map<String, Product> productRepo = new HashMap<>();
+   static {
+      Product honey = new Product();
+      honey.setId("1");
+      honey.setName("Honey");
+      productRepo.put(honey.getId(), honey);
+      
+      Product almond = new Product();
+      almond.setId("2");
+      almond.setName("Almond");
+      productRepo.put(almond.getId(), almond);
+   }
+   
+   @RequestMapping("/")
+   String home() {
+       return "Hello World!";
+   }
+   
+   @RequestMapping(value = "/products")
+   public ResponseEntity<Object> getProducts() {
+      return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+   }
+   
+}
